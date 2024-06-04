19dc0e3c7f9d0a3999e64d4aa73305fae1cc21c2
nclslbrn <nclslbrn@gmail.com>
2024-06-04 18:51:45 +0200

    better but still unfinished

--- example/letter-in-grid/src/main.ts
+++ example/letter-in-grid/src/main.ts
@@ -1,21 +1,71 @@
 import './style.css';
 import p5 from 'p5';
-import { type, Glyph, Line, Vec, getGlyphVector } from '@nclslbrn/plot-writer';
+import { type Line, Vec, getGlyphVector } from '@nclslbrn/plot-writer';
 
-let path = [];
-const sentences = [...'They devour that by which they themselves wish to be devoured.'];
-const sketch = (p5) => {
-  let cells = [[0, 0, 1, 1]];
+let paths = [] as Line[],
+  readCursor = 0;
+const sentence = [...'They devour that by which they themselves wish to be devoured.'];
+const chars = [
+  ':.:.:.:  .  ',
+  '*  ^  *  ^  ',
+  '__=-___==--_',
+  '//  //  //  ',
+  '° . ° . ° . ',
+  '/\\-|-|',
+  '////L__',
+  '#------',
+  '*-^-^-^-',
+  'L____n ',
+  'ʌ/V\\ʌ________',
+  '|___oxo___',
+  'x|___\\|/__|',
+  '|+--=--+',
+];
+
+const getSVG = () => {
+  const data = new Blob(
+    [
+      `<?xml version="1.0" encoding="UTF-8" standalone="no"?>\r\n` +
+        `<svg width="${window.innerWidth}" height="${window.innerHeight}" viewbox="0 0 ${window.innerWidth} ${window.innerHeight}">\r\n\t` +
+        `<style> path { fill: none; stroke: #333; }</style>\r\n\t\t` +
+        paths.reduce(
+          (lines: string, line: Line) =>
+            `${lines}<path d="${line.reduce((d, v, i) => `${d}${i === 0 ? 'M' : 'L'}${v[0]},${v[1]} `, '')}"/>\r\n\t\t`,
+          ''
+        ) +
+        `\r\n\t</svg>`,
+    ],
+    { type: 'image/svg+xml' }
+  );
+
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(data);
+  link.download = `letter-in-grid-${new Date().toISOString()}.svg`;
+  link.click();
+};
+
+const sketch = (p: p5) => {
+  let cells = [[0, 0, 1, 1]],
+    sentencePrinted = false;
+  const baseSize = [12, 18];
+  const debug = false;
+
   const expand = (d: number, i: number) =>
     i % 2 === 0
-      ? p5.map(d, 0, 1, 20, window.innerWidth - 40)
-      : p5.map(d, 0, 1, 20, window.innerHeight - 40);
+      ? p.map(d, 0, 1, 20, window.innerWidth - 40)
+      : p.map(d, 0, 1, 20, window.innerHeight - 40);
 
   const splitCell = () => {
-    const toSplit = p5.floor(p5.random(cells.length));
+    let toSplit = -1;
+    while (toSplit < 0) {
+      const randCell = p.floor(p.random(cells.length));
+      if (cells[randCell][2] > 0.166 && cells[randCell][3] > 0.166) {
+        toSplit = randCell;
+      }
+    }
     const [x, y, w, h] = cells[toSplit];
-    const splitAt = p5.random([0.166, 0.333, 0.5, 0.666, 0.833]);
-    const splitOn = p5.random() > 0.5 ? 'x' : 'y';
+    const splitAt = p.random([0.166, 0.333, 0.5, 0.666, 0.833]);
+    const splitOn = p.random() > 0.5 ? 'x' : 'y';
     const splitted = [];
     if (splitOn === 'x') {
       splitted.push(
@@ -36,26 +86,60 @@ const sketch = (p5) => {
     cells.push(...splitted);
   };
 
-  p5.setup = function () {
-    p5.createCanvas(window.innerWidth, window.innerHeight);
-    p5.background('white');
-    p5.fill('tomato');
-    p5.stroke('white');
-    while (cells.length < sentences.length) splitCell();
+  const fillCell = (cell: number[]) => {
+    const scale = p.random([0.5, 1, 2]);
+    const letterSize = baseSize.map((d) => d * scale);
+    const [x, y, w, h] = cell.map((d, i) => expand(d, i));
+    debug &&
+      paths.push([
+        [x, y],
+        [x + w, y],
+        [x + w, y + h],
+        [x, y + h],
+        [x, y],
+      ] as Line);
+    const sample = p.random() > 0.66 || sentencePrinted ? [...p.random(chars)] : sentence;
+    if (sample === sentence) sentencePrinted = true;
+    for (let ly = baseSize[1]; ly < h - baseSize[1] * 2; ly += letterSize[1]) {
+      for (let lx = baseSize[0]; lx < w - baseSize[0] * 2; lx += letterSize[0]) {
+        const tIdx = readCursor % sample.length;
 
-    cells.forEach((c, i) => {
-      const [x, y, w, h] = c.map((d, i) => expand(d, i));
-      p5.rect(x, y, w, h);
-      const letter = getGlyphVector(sentences[i % sentences.length], [w, h], [x, y]);
-      letter.forEach((line: Line) => {
-        p5.beginShape();
-        line.forEach((p: Vec) => p5.vertex(...p));
-        p5.endShape();
-      });
+        if (sample[tIdx] !== ' ') {
+          const glyph = getGlyphVector(sample[tIdx], letterSize, [x + lx, y + ly]);
+          glyph.forEach((line: Line) => paths.push(line));
+        }
+        if (sample !== sentence && p.random() > 0.66) {
+          if (p.random() > 0.5) {
+            readCursor--;
+          }
+        } else {
+          readCursor++;
+        }
+      }
+    }
+  };
+
+  p.setup = function () {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.background('white');
+    p.noFill();
+    p.stroke('#333');
+    for (let i = 0; i < 12; i++) {
+      splitCell();
+    }
+
+    cells.forEach((c) => fillCell(c));
+
+    paths.forEach((l: Line) => {
+      p.beginShape();
+      l.forEach((pt: Vec) => p.vertex(pt[0], pt[1]));
+      p.endShape();
     });
   };
-};
 
-// const SVG = () => {}
+  p.keyPressed = function (e: KeyboardEvent) {
+    if (e.key === 'd') getSVG();
+  };
+};
 
 new p5(sketch, document.getElementById('app')!);
