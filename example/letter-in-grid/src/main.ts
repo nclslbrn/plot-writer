0363acc8276434c5efd073e0bf8bcdbea45753a3
Nicolas Lebrun <nl@artemg.com>
2024-06-06 00:17:56 +0200

    more random text

--- example/letter-in-grid/src/main.ts
+++ example/letter-in-grid/src/main.ts
@@ -4,7 +4,9 @@ import { type Line, Vec, getGlyphVector } from '@nclslbrn/plot-writer';
 
 let paths = [] as Line[],
   readCursor = 0;
-const sentence = [...'They devour that by which they themselves wish to be devoured.'];
+const sentence = [
+  ...'They devour that by which they themselves wish to be devoured.'.toUpperCase(),
+];
 const chars = [
   ':.:.:.:  .  ',
   '*  ^  *  ^  ',
@@ -30,7 +32,7 @@ const getSVG = () => {
         `<style> path { fill: none; stroke: #333; }</style>\r\n\t\t` +
         paths.reduce(
           (lines: string, line: Line) =>
-            `${lines}<path d="${line.reduce((d, v, i) => `${d}${i === 0 ? 'M' : 'L'}${v[0]},${v[1]} `, '')}"/>\r\n\t\t`,
+            `${lines}<path d="${line.reduce((d: string, v: Vec, i: number) => `${d}${i === 0 ? 'M' : 'L'}${v[0]},${v[1]} `, '')}"/>\r\n\t\t`,
           ''
         ) +
         `\r\n\t</svg>`,
@@ -46,25 +48,20 @@ const getSVG = () => {
 
 const sketch = (p: p5) => {
   let cells = [[0, 0, 1, 1]],
-    sentencePrinted = false;
-  const baseSize = [12, 18];
+    sentencePrinted = false,
+    absrctTxt = [...p.random(chars)];
+  const baseSize = [7, 10];
   const debug = false;
 
-  const expand = (d: number, i: number) =>
+  const expand = (d: number, i: number): number =>
     i % 2 === 0
       ? p.map(d, 0, 1, 20, window.innerWidth - 40)
       : p.map(d, 0, 1, 20, window.innerHeight - 40);
 
-  const splitCell = () => {
-    let toSplit = -1;
-    while (toSplit < 0) {
-      const randCell = p.floor(p.random(cells.length));
-      if (cells[randCell][2] > 0.166 && cells[randCell][3] > 0.166) {
-        toSplit = randCell;
-      }
-    }
+  const splitCell = (): void => {
+    const toSplit = p.floor(p.random(cells.length));
     const [x, y, w, h] = cells[toSplit];
-    const splitAt = p.random([0.166, 0.333, 0.5, 0.666, 0.833]);
+    const splitAt = p.random([0.333, 0.5, 0.666]);
     const splitOn = p.random() > 0.5 ? 'x' : 'y';
     const splitted = [];
     if (splitOn === 'x') {
@@ -86,8 +83,23 @@ const sketch = (p: p5) => {
     cells.push(...splitted);
   };
 
-  const fillCell = (cell: number[]) => {
-    const scale = p.random([0.5, 1, 2]);
+  const noiseLine = (line: Line): Line => {
+    const noised = [] as Line;
+    for (let i = 0; i < line.length - 1; i++) {
+      const distance = Math.hypot(line[i + 1][0] - line[i][0], line[i + 1][1] - line[i][1]);
+      const angle = Math.atan2(line[i + 1][1] - line[i][1], line[i + 1][0] - line[i][0]);
+      for (let d = 0; d < distance; d++) {
+        const pos = [line[i][0] + Math.cos(angle) * d, line[i][1] + Math.sin(angle) * d];
+        const def = p.noise(...pos.map((v) => v * 0.007)) * Math.PI;
+        const strenght = (d / distance - 0.5) * 0.2 * distance;
+        noised.push([pos[0] + Math.cos(def) * strenght, pos[1] + Math.sin(def) * strenght]);
+      }
+    }
+    return noised;
+  };
+
+  const fillCell = (cell: number[]): void => {
+    const scale = p.random([0.5, 1, 3]);
     const letterSize = baseSize.map((d) => d * scale);
     const [x, y, w, h] = cell.map((d, i) => expand(d, i));
     debug &&
@@ -98,14 +110,19 @@ const sketch = (p: p5) => {
         [x, y + h],
         [x, y],
       ] as Line);
-    const sample = p.random() > 0.66 || sentencePrinted ? [...p.random(chars)] : sentence;
-    if (sample === sentence) sentencePrinted = true;
-    for (let ly = baseSize[1]; ly < h - baseSize[1] * 2; ly += letterSize[1]) {
-      for (let lx = baseSize[0]; lx < w - baseSize[0] * 2; lx += letterSize[0]) {
+    let sample = p.random() > 0.66 ? absrctTxt : sentence;
+    //if (sample === sentence) sentencePrinted = true;
+    for (let ly = baseSize[1] * 2; ly < h - baseSize[1] * 2; ly += letterSize[1] * 2) {
+      for (let lx = baseSize[0] * 2; lx < w - baseSize[0] * 2; lx += letterSize[0] * 2) {
         const tIdx = readCursor % sample.length;
+        const dy = (p.noise(lx / w, ly) - 0.5) * baseSize[1] * 5;
 
         if (sample[tIdx] !== ' ') {
-          const glyph = getGlyphVector(sample[tIdx], letterSize, [x + lx, y + ly]);
+          const glyph = getGlyphVector(
+            sample[tIdx],
+            letterSize.map((d) => d * 1.2),
+            [x + lx, y + ly + dy]
+          );
           glyph.forEach((line: Line) => paths.push(line));
         }
         if (sample !== sentence && p.random() > 0.66) {
@@ -113,7 +130,15 @@ const sketch = (p: p5) => {
             readCursor--;
           }
         } else {
-          readCursor++;
+          if (sample !== sentence) {
+            readCursor++;
+          } else {
+            if (readCursor < sentence.length - 1) {
+              readCursor++;
+            } else {
+              sample = [...p.random(chars)];
+            }
+          }
         }
       }
     }
@@ -124,7 +149,8 @@ const sketch = (p: p5) => {
     p.background('white');
     p.noFill();
     p.stroke('#333');
-    for (let i = 0; i < 12; i++) {
+    p.strokeWeight(2);
+    for (let i = 0; i < 22; i++) {
       splitCell();
     }
 
@@ -139,6 +165,7 @@ const sketch = (p: p5) => {
 
   p.keyPressed = function (e: KeyboardEvent) {
     if (e.key === 'd') getSVG();
+    if (e.key === 'p') p.saveCanvas(`letter-in-grid-${new Date().toISOString()}`, 'jpg');
   };
 };
 
