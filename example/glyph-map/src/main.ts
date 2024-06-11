082064b69df29548768ca1d99f123da3bcbf8cc1
nclslbrn <nclslbrn@gmail.com>
2024-06-11 19:04:28 +0200

    last adjustements before pen plotting

--- example/glyph-map/src/main.ts
+++ example/glyph-map/src/main.ts
@@ -20,20 +20,22 @@ const DPI = quantity(100, 'dpi'),
   MRGN = convert(mul(quantity(20, 'mm'), DPI), NONE),
   BASE = [12.1, 14].map((d) => convert(mul(quantity(d, 'mm'), DPI), NONE)),
   EXTS = [4.25, 5.5].map((d) => convert(mul(quantity(d, 'mm'), DPI), NONE)),
-  THRD = (SIZE[0] - MRGN * 2) / 3;
+  WEIGHTS = [0.7, 0.3].map((d) => convert(mul(quantity(d, 'mm'), DPI), NONE)),
+  THRD = (SIZE[0] - MRGN * 2) / 3;;
 
 const init = () => {
   cnvs = <HTMLCanvasElement>document.getElementById('main')!;
   const ctx = cnvs!.getContext('2d'),
     dpr = adaptDPI(cnvs, ...SIZE),
-    glyphs = [] as polyline[];
+    largeGlyphs = [] as polyline[],
+    smallGlyphs = [] as polyline[];
 
   letters.forEach((l, i) => {
     const bx = MRGN + BASE[0] / 2 + (i % 3) * BASE[0] * 7;
     const by = MRGN + BASE[1] / 2 + Math.floor(i / 3) * BASE[1] * 1.25;
     // base letters
     getGlyphVector(l[0], BASE, [bx, by]).map((line: Line) => {
-      glyphs.push(polyline(line));
+      largeGlyphs.push(polyline(line));
     });
 
     l.splice(0, 1);
@@ -43,9 +45,8 @@ const init = () => {
       const ex = bx + BASE[0] * 1.25 + (j % lnRetAt) * EXTS[0];
       const ey = by + Math.floor(j / lnRetAt) * EXTS[1] + cy;
       getGlyphVector(l[j], EXTS, [ex, ey]).map((line: Line) => {
-        glyphs.push(polyline(line));
+        smallGlyphs.push(polyline(line));
       });
-      glyphs;
     }
   });
 
@@ -54,7 +55,7 @@ const init = () => {
       MRGN + BASE[0] / 2 + BASE[0] * (i % 6),
       SIZE[1] - MRGN * 3.25 + BASE[1] * Math.floor(i / 6),
     ]).map((line: Line) => {
-      glyphs.push(polyline(line));
+      largeGlyphs.push(polyline(line));
     });
   });
 
@@ -63,7 +64,7 @@ const init = () => {
       MRGN + BASE[0] * 6.3 + BASE[0] * 1.25 + (i % 18) * EXTS[0],
       SIZE[1] - MRGN * 3.12 + Math.floor(i / 18) * EXTS[1],
     ]).map((line: Line) => {
-      glyphs.push(polyline(line));
+      smallGlyphs.push(polyline(line));
     });
   });
 
@@ -72,37 +73,33 @@ const init = () => {
       MRGN + BASE[0] * 13.3 + BASE[0] * 1.25 + (i % 18) * EXTS[0],
       SIZE[1] - MRGN * 3.12 + Math.floor(i / 18) * EXTS[1],
     ]).map((line: Line) => {
-      glyphs.push(polyline(line));
+      smallGlyphs.push(polyline(line));
     });
   });
 
-  [...'Plot-writer / A SVG text writer for plotter (MIT License) Nicolas LEBRUN'].forEach(
+  [...'Plot-writer / An SVG text writer for plotter (MIT License) Nicolas LEBRUN'].forEach(
     (l, i) => {
       getGlyphVector(
         l,
         [EXTS[0] * 0.84, EXTS[1]],
-        [MRGN + i * EXTS[0] * 0.84, SIZE[1] - MRGN * 1.5]
+        [MRGN + i * EXTS[0] * 0.812, SIZE[1] - MRGN * 1.5]
       ).map((line: Line) => {
-        glyphs.push(polyline(line));
+        smallGlyphs.push(polyline(line));
       });
     }
   );
-  comp = group({}, [
-    rect(SIZE, { fill: '#fefefe' }),
-    /*
-    rect(
-      [MRGN, MRGN],
-      SIZE.map((d: number) => d - MRGN * 2),
-      { fill: 'none', stroke: '#333' }
-    ),
+  comp = group({ stroke: "#FEFEFE"}, [
+    rect(SIZE, { fill: '#333' }),
+
     line([MRGN + THRD, MRGN + BASE[1] / 2], [MRGN + THRD, SIZE[1] - (MRGN + BASE[1] * 4)]),
     line([MRGN + THRD * 2, MRGN + BASE[1] / 2], [MRGN + THRD * 2, SIZE[1] - (MRGN + BASE[1] * 4)]),
 
     line([MRGN, SIZE[1] - MRGN * 3.5], [SIZE[0] - MRGN, SIZE[1] - MRGN * 3.5]),
     line([MRGN, SIZE[1] - MRGN * 1.6], [SIZE[0] - MRGN, SIZE[1] - MRGN * 1.6]),
-    */
+    
 
-    group({ stroke: '#333' }, glyphs),
+    group({ weight: WEIGHTS[0] }, largeGlyphs),
+    group({ weight: WEIGHTS[1] }, smallGlyphs),
   ]);
   ctx!.scale(dpr, dpr);
   draw(ctx, comp);
